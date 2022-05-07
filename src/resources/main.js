a16b24210e7be780766e0c83642006f06c5c04e8
Hélder Silva Ferreira Lima <74564010+heldaolima@users.noreply.github.com>
2022-05-07 11:47:48 -0300

    atualizando

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,67 +1,31 @@
-var form = document.getElementById('formCliente');
-form.onsubmit = function(){
-    var xhr = new XMLHttpRequest();
-    var formData = new FormData(form);
-    xhr.open('POST', form.action);
+var formPost = document.getElementsByClassName('formPost');
 
-    xhr.send(JSON.stringify(Object.fromEntries(formData)));
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            form.reset();
+for (let i = 0; i < formPost.length; i++) {
+    formPost[i].onsubmit = function(){
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData(formPost[i]);
+        xhr.open('POST', formPost[i].action);
+    
+        xhr.send(JSON.stringify(Object.fromEntries(formData)));
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                formPost[i].reset();
+                
+            }
         }
+        return false;
     }
-    return false;
 }
 
-
-
-// function ajusteFloat(id) {
-//     var meuFloat = document.getElementById(id);
-//     console.log(meuFloat.value);
-//     // meuFloat.value = parseFloat(meuFloat);
-// }
-
-/*
-
-const formCliente = document.querySelector("#formCliente");
-
-formCliente.addEventListener('submit', async function(e) {
-    e.preventDefault(); //nao enviar o form antes dele ser tratado
-    const form = e.currentTarget;
-
-    const url = form.action;
-
-    try {
-        const formData = new FormData(form);
-        const responseData = await postFormDataAsJson({url, formData});
-        console.log({responseData});
-    } catch(error) {
-        console.log(error);
-    }
-
+var url = "http://localhost:8080/clientes";
+var getclbt = document.getElementById('getClientes');
+getclbt.addEventListener('click', function(){
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    }).catch(function() {
+        console.log("booo");
+    });
 });
-
-async function postFormDataAsJson({url, formData}) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    const formDataJsonString = JSON.stringify(plainFormData);
-
-    const fetchOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: formDataJsonString,
-    };
-
-    const response = await fetch(url, fetchOptions);
-
-    if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
-    return response.json();
-}
-*/
-
